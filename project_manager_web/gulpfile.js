a310203ecdd91115fe8d4505cab4476293ae41d2
Marc-Andre Boily <mandre.boily@gmail.com>
2016-11-10 22:59:01 -0500

    Added livereload to gulpfile, Styling for tables and buttons

--- project_manager_web/gulpfile.js
+++ project_manager_web/gulpfile.js
@@ -3,11 +3,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass', function () {
     return gulp.src('./sass/app.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./static/'));
+        .pipe(gulp.dest('./static/'))
+        .pipe(livereload());
 });
 
 gulp.task('sass:watch', function() {
@@ -15,3 +17,17 @@ gulp.task('sass:watch', function() {
         gulp.start('sass');
     });
 })
+
+gulp.task('livereload', function() {
+    livereload.listen({ start: true});
+
+    // SASS
+    watch('./sass/**/*.scss', function () {
+        gulp.start('sass');
+    });
+
+    // Templates
+    watch('../templates/**/*.html', function () {
+        livereload.changed('../templates/');
+    });
+});
